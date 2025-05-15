5b8df1023eb0958c038d05e773de53ae9d96b89f
Nopalogic <naufaladhi02@gmail.com>
2025-05-15 18:18:56 +0700

    chore(structure): reorganize auth files

--- frontend/src/routes/auth/register.tsx
+++ frontend/src/routes/auth/register.tsx
@@ -1,4 +1,4 @@
-import RegisterPage from '@/pages/register'
+import RegisterPage from '@/pages/auth/register'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/auth/register')({
