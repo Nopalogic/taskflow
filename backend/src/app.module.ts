5cb7c7e724edfd077b912c0eaad422a29f44d6c4
Nopalogic <naufaladhi02@gmail.com>
2025-11-27 23:47:19 +0700

    build(db): set up prisma ORM

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { PrismaModule } from './core/prisma.module';
+import { PrismaModule } from './core/database/prisma.module';
 
 @Module({
   imports: [
