d9546c9e93f108978bbb0342ad14e73c08d68bbe
Nopalogic <naufaladhi02@gmail.com>
2025-11-28 21:03:39 +0700

    fix(workspace): import prisma module in workspace module file

--- backend/src/modules/workspace/workspace.module.ts
+++ backend/src/modules/workspace/workspace.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { WorkspaceService } from './workspace.service';
 import { WorkspaceController } from './workspace.controller';
 import { CreateWorkspaceHook } from './workspace.hook';
+import { PrismaModule } from 'src/core/database/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [WorkspaceController],
   providers: [CreateWorkspaceHook, WorkspaceService],
 })
